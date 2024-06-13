f442416b619a88ad04a8d79b359fab042a39c667
EmersonW Silva <emrsnwslv@gmail.com>
2024-06-13 20:38:05 -0300

    Desafio ListaTarefa

---
+++ src/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,33 @@
+package list.OperacoesBasicas;
+
+public class Tarefa {
+  private String descricao;
+
+  public Tarefa(String descricao) {
+    this.descricao = descricao;
+  }
+
+  public String getDescricao() {
+    return descricao;
+  }
+
+  @Override
+  public String toString() {
+    return "Tarefa [descricao=" + descricao + "]";
+  }
+
+  public static void main(String[] args) {
+    ListaTarefa listaTarefa = new ListaTarefa();
+    System.out.println("O numero total de elementos na lista e: " + listaTarefa.obterNumeroTotalTarefas());
+
+    listaTarefa.adicionarTarefa("Tarefa 1");
+    listaTarefa.adicionarTarefa("Tarefa 1");
+    listaTarefa.adicionarTarefa("Tarefa 2");
+    System.out.println("O numero total de elementos na lista e: " + listaTarefa.obterNumeroTotalTarefas());
+
+    listaTarefa.removerTarefa("Tarefa 2");
+    System.out.println("O numero total de elementos na lista e: " + listaTarefa.obterNumeroTotalTarefas());
+
+    listaTarefa.obterDescricoesTarefas();
+  }
+}
